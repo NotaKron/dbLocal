50f6ca5e459f17c546b1a3a805b0cfdfe25130cc
NotaKron <elf.lorien@mail.ru>
2016-06-08 15:40:18 +0300

    Проверка

--- classes/Modules/modules.php
+++ classes/Modules/modules.php
@@ -16,79 +16,101 @@ class Modules {
     private $countRow;
     private $query;
     private $arrayFilteredColumns;
+    private $arrayDefaultValues;
     function __construct($res,$query) {
-     $this->_res=$res;
-     $this->countRow=0;
-     $this->query=$query;
-     $this->arrayFilteredColumns =$this->getFilteredColumns();  
+        $this->_res=$res;
+        $this->countRow=0;
+        $this->query=$query;
+        $this->arrayFilteredColumns =$this->getFilteredColumnsArray();
+        $this->arrayDefaultValues=$this->getDefaultArray();
     }
     //put your code here
     function printTable($res){
-                echo '<table cellpadding="5" cellspacing="0" border="1">';
- echo '<tr>';
-$_tmp=$res[0];
-$_tmpkey=  array_keys($_tmp);
-foreach ($_tmpkey as $key => $value){
-    echo "<th> $value</th>"; 
-}
-echo '</tr>';
-foreach ($res as $key => $value) {
-    echo "<tr>";
-    foreach ($value as $data)
-        
-        if(is_a($data, 'DateTime'))
-           {
-           $res= $data->format('Y-m-d H:i:s');
-             echo "<td>".$res."</td>";
-            }else{
-            echo "<td>".$data."</td>";}
-    echo "</tr>";
-}
-echo "</table>";
+        echo '<table cellpadding="5" cellspacing="0" border="1">';
+        echo '<tr>';
+        $_tmp=$res[0];
+        $_tmpkey=  array_keys($_tmp);
+        foreach ($_tmpkey as $key => $value){
+            echo "<th> $value</th>";
+        }
+        echo '</tr>';
+        foreach ($res as $key => $value) {
+            echo "<tr>";
+            foreach ($value as $data)
+
+                if(is_a($data, 'DateTime'))
+                {
+                    $res= $data->format('Y-m-d H:i:s');
+                    echo "<td>".$res."</td>";
+                }else{
+                    echo "<td>".$data."</td>";}
+            echo "</tr>";
+        }
+        echo "</table>";
     }
-    function filteredArray($res){
+    private function filteredArray($columnName,$value){
+        //
+    }
+    function getTableHeaders(){
+        $_tmp=$this->_res[0];
+        $_tmpkey=  array_keys($_tmp);
+        print_r($_tmpkey);
+    }
+    private function  getFilteredColumnsArray()
+    {
+        preg_match_all('/\"(.*?)\"/sei', $this->query,$matches);
+        return $matches[1];
+    }
+    function printRows(){
+        foreach ($this->_res as $key=>$value)
+        {
+            $this->parceRow($value);
+        }
+    }
+    private function parceRow($row)
+    {
+        foreach ($row as $cell=>$key) {
+            $keyRow= array_search($key,$row);
+            if(in_array($keyRow,$this->arrayFilteredColumns)){
+                $this->countRepeats($keyRow);
+            }
+            else echo $keyRow.' не фильтруем! <br>';
+        }
+    }
+    function test()
+    {
+        $this->printRows();
+    }
+    private function filteredColumns($columnName,$cell){
+        $sizeArray=count($this->arrayFilteredColumns);
+
+        for($this->countRow;$this->countRow<$sizeArray;++$this->countRow){
+            if(key($this->arrayDefaultValues[$columnName])==$cell){
+              // $countRepeats=
+            }
+        }
+    }
+    private function getDefaultArray(){
+        foreach ($this->arrayFilteredColumns as $key){
+            $this->arrayDefault[$key]=['default'=>0];
+        }
+        return $this->arrayDefault;
+    }
+    private function countRepeats($columnName){
         $count=0;
         $uniqString=NULL;
-        foreach ($res as $key=>$value){
-            if($uniqString==$value['ORDER_CATEGORY'] )
+        foreach ($this->_res as $key=>$value){
+            if($uniqString==$value[$columnName] )
             {
                 $count++;
             }
             elseif ($uniqString!=NULL) {
                 echo $uniqString.' повторятется '.$count.'<br>';
-                $uniqString=$value['ORDER_CATEGORY'];
+                $uniqString=$value[$columnName];
                 $count=0;
-             } else {
-                 $uniqString=$value['ORDER_CATEGORY'];
-             }
-        }
-           }
-    function getTableHeaders(){
-        $_tmp=$this->_res[0];
-        $_tmpkey=  array_keys($_tmp);
-        print_r($_tmpkey);
-    } 
-    private function  getFilteredColumns()
-    {
-        preg_match_all('/\"(.*?)\"/sei', $this->query,$matches);
-        return $matches[1];
-    }
-    function printRows($nameColumn){
-        foreach ($this->_res as $key=>$value)
-        {
-            echo "Это строка №: $this->countRow ";
-            print_r($value);
-            echo '<br>';
-            $this->countRow++;
+            } else {
+                $uniqString=$value[$columnName];
+            }
         }
     }
-    private function parceRow($row)
-    {
-      #  $filtColumn=  $this->getFilteredColumns(f)
-    }
-    function test()
-    {
-        print_r($this->arrayFilteredColumns);
-    }
-           
 }
